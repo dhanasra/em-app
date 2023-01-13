f3afb0f4f5383033ea5b13bfa7c9e094e2c0e2e8
dhanasekaran <dhana.spiderlingz@gmail.com>
2023-01-13 16:40:30 +0530

    docs : hive added for local db

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,8 +1,12 @@
 
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 final instance = GetIt.instance;
 
 Future<void> initAppModule() async{
+
+  await Hive.initFlutter();
+
 
 }
