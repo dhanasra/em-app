b57fc574225d84c232d15ca61a0ff2de55fa3737
dhanasekaran <dhana.spiderlingz@gmail.com>
2023-01-18 23:40:08 +0530

    feat : init hive di & firebase

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,8 +1,11 @@
 
+import 'dart:io';
+
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
-import '../network/firebase.dart';
+import '../network/config/firebase.dart';
 
 final instance = GetIt.instance;
 
@@ -14,5 +17,17 @@ Future<void> initAppModule() async{
   //Firebase init
   instance.registerLazySingleton<FirebaseClient>(() => FirebaseClient());
   await instance.get<FirebaseClient>().init();
+
+  Directory appDocDirectory = await getApplicationDocumentsDirectory();
+
+  //Hive Box open
+  final boxCollection = await BoxCollection.open(
+    'ExpensoBox', 
+    {'expenses', 'settings'}, 
+    path: appDocDirectory.path
+  );
+  final expenseBox = await boxCollection.openBox<Map>('expenses');
+
+  instance.registerLazySingleton<CollectionBox>(() => expenseBox, instanceName: 'expenses');
 
 }
